b14a79c9cba1ca32a10898fbad3a9cfb6f192cab
rgiinther <r.giin95@gmail.com>
2022-07-17 15:51:13 -0500

    completed idb js

---
+++ Develop/public/js/idb.js
@@ -0,0 +1,64 @@
+let db;
+
+
+const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('newBudget', { autoIncrement: true});
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+//check if app is online
+    if (navigator.onLine) {
+        uploadItem();
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+//submit a new transaction with no internet
+function saveRecord(record) {
+    const transaction = db.transaction(['newBudget'], 'readwrite');
+    const itemObjectStore = transaction.objectStore('newBudget');
+    itemObjectStore.add(record);
+};
+
+function uploadItem() {
+    const transaction = db.transaction(["newBudget"], "readwrite");
+    const itemObjectStore = transaction.objectStore("newBudget");
+  
+    const getAll = itemObjectStore.getAll();
+  
+    getAll.onsuccess = function () {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => response.json())
+          .then((serverResponse) => {
+            if (serverResponse.message) {
+              throw new Error(serverResponse);
+            }
+            const transaction = db.transaction(["newBudget"], "readwrite");
+            const itemObjectStore = transaction.objectStore("newBudget");
+            itemObjectStore.clear();
+            alert("All transactions submitted");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    };
+  }
+  
+  window.addEventListener("online", uploadItem);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # 19-travel-budget-PWA
+
+User story
+
+AS AN avid traveler
+I WANT to be able to track my withdrawals and deposits with or without a data/internet connection
+SO THAT my account balance is accurate when I am traveling 
+
+
+Technologies used: 
+node js
+java script 
+
+
+
+acceptance criteria: 
+GIVEN a budget tracker without an internet connection
+WHEN the user inputs an expense or deposit
+THEN they will receive a notification that they have added an expense or deposit
+WHEN the user reestablishes an internet connection
+THEN the deposits or expenses added while they were offline are added to their transaction history and their totals are updated
